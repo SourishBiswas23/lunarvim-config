004f9627df65595084fa944d0a6b9575ea411967
SourishBiswas23 <sourish135@gmail.com>
2025-01-03 21:31:05 +0530

    Can now use alt+1/2/3 to go to first, second and third tab

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,4 +1,6 @@
-print('This is the plugin directory')
+vim.keymap.set('n', "<M-1>", "<cmd>BufferLineGoToBuffer 1<CR>")
+vim.keymap.set('n', "<M-2>", "<cmd>BufferLineGoToBuffer 2<CR>")
+vim.keymap.set('n', "<M-3>", "<cmd>BufferLineGoToBuffer 3<CR>")
 
 for i = 1, 9 do
   lvim.keys.normal_mode["<M-" .. i .. ">"] = "<Cmd>BufferLineGoToBuffer " .. i .. "<CR>"
