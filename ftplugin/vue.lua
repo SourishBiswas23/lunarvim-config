235101db5f17d1c379b3db91ca89c05a0c6f2818
SourishBiswas23 <sourish135@gmail.com>
2025-01-20 16:19:34 +0530

    Now using both vue-language-server and tsserver for .vue files

--- ftplugin/vue.lua
+++ ftplugin/vue.lua
@@ -3,11 +3,29 @@ require("lvim.lsp.manager").setup('emmet_ls', {})
 require("lvim.lsp.manager").setup('eslint', {})
 require("lvim.lsp.manager").setup('tailwindcss', {})
 require("lvim.lsp.manager").setup('volar', {
+  filetypes = { 'vue' },
   init_options = {
     vue = {
       hybrid_mode = false
     }
   }
+})
+
+local mason_registry = require("mason-registry")
+local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+    .. "/node_modules/@vue/language-server"
+
+require("lvim.lsp.manager").setup('tsserver', {
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server_path,
+        languages = { "vue" },
+      },
+    },
+  },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
 })
 
 -- Formatters
